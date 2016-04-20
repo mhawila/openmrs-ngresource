c572d5b2be5a2bba9ee6a6264fd8072d69e380a4
mhawila <a.mhawila@gmail.com>
2016-04-20 16:04:51 +0300

    Adding Assessment concept uuid

--- app/scripts/module/utils/constants/concept-uuid.const.js
+++ app/scripts/module/utils/constants/concept-uuid.const.js
@@ -16,6 +16,7 @@
         CUR_TB_TX: 'a899e444-1350-11df-a1f1-0026b9348838',
         TB_TX_DRUG_STARTED_DETAILED: 'a89fe6f0-1350-11df-a1f1-0026b9348838',
         TB_TX_PLAN: 'a89c1fd4-1350-11df-a1f1-0026b9348838',
-        CC_HPI: 'a89ffbf4-1350-11df-a1f1-0026b9348838'
+        CC_HPI: 'a89ffbf4-1350-11df-a1f1-0026b9348838',
+        ASSESSMENT: '23f710cc-7f9c-4255-9b6b-c3e240215dba'
       });  
 })();
